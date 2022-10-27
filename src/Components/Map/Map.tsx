9a157df395074bd4876892e77063490b7b35d81b
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-27 23:25:02 +0300

    listRefs

---
+++ src/Components/Map/Map.tsx
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+
+export const Map = () => {
+  const [coords, setCoords] = useState({ lat: -0.7112125, lng: 36.9930926 });
+  return <div>Map</div>;
+};
+export default Map;
