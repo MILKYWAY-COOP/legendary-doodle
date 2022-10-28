e8b49bbc7781600cd88149ebdca9b78198472aff
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-28 23:11:16 +0300

    Updates

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,56 +1,61 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { NavbarContainer } from './Navbar.styled';
 
 const Navbar = () => {
+  const [list, setList] = useState<Element[]>([]);
+
   const navRef = useRef<HTMLDivElement>(null);
   const listRef = useRef<HTMLUListElement>(null);
   const backgroundRef = useRef<HTMLDivElement>(null);
 
-  const listChildren = listRef.current?.children;
   const background = backgroundRef.current;
   const nav = navRef.current;
 
-  if (listChildren) {
-    const listChildrenArray = Array.from(listChildren);
+  list.forEach((listItem) => {
+    if (listItem.className === 'not') return;
+    listItem.addEventListener('mouseenter', (e) => {
+      const target = e.target as HTMLElement;
+      const dropdown = target.querySelector('.dropdown') as HTMLElement;
+      const dropdownCoords = dropdown?.getBoundingClientRect();
+      const navCoords = nav?.getBoundingClientRect();
+      const left = navCoords ? navCoords.left : 0;
 
-    listChildrenArray.forEach((listItem) => {
-      listItem.addEventListener('mouseenter', (e) => {
-        const target = e.target as HTMLElement;
-        const dropdown = target.querySelector('.dropdown') as HTMLElement;
-        const dropdownCoords = dropdown?.getBoundingClientRect();
-        const navCoords = nav?.getBoundingClientRect();
-        const top = navCoords ? navCoords.top : 0;
-        const left = navCoords ? navCoords.left : 0;
+      const coords = {
+        height: dropdownCoords.height,
+        width: dropdownCoords.width,
+        top: dropdownCoords.top,
+        left: dropdownCoords.left - left
+      };
 
-        const coords = {
-          height: dropdownCoords.height,
-          width: dropdownCoords.width,
-          top: dropdownCoords.top - top,
-          left: dropdownCoords.left - left
-        };
+      background?.style.setProperty('width', `${coords.width}px`);
+      background?.style.setProperty('height', `${coords.height}px`);
+      background?.style.setProperty(
+        'transform',
+        `translate(${coords.left}px, ${coords.top}px)`
+      );
 
-        background?.style.setProperty('width', `${coords.width}px`);
-        background?.style.setProperty('height', `${coords.height}px`);
-        background?.style.setProperty(
-          'transform',
-          `translate(${coords.left}px, ${coords.top}px)`
-        );
-
-        target.classList.add('trigger-enter');
-        setTimeout(() => {
-          target.classList.contains('trigger-enter') &&
-            target.classList.add('trigger-enter-active');
-        }, 150);
-        background?.classList.add('open');
-      });
-
-      listItem.addEventListener('mouseleave', (e) => {
-        const target = e.target as HTMLElement;
-        target.classList.remove('trigger-enter', 'trigger-enter-active');
-        background?.classList.remove('open');
-      });
+      target.classList.add('trigger-enter');
+      setTimeout(() => {
+        target.classList.contains('trigger-enter') &&
+          target.classList.add('trigger-enter-active');
+      }, 150);
+      background?.classList.add('open');
     });
-  }
+
+    listItem.addEventListener('mouseleave', (e) => {
+      const target = e.target as HTMLElement;
+      target.classList.remove('trigger-enter', 'trigger-enter-active');
+      background?.classList.remove('open');
+    });
+  });
+
+  useEffect(() => {
+    const listChildren = listRef.current?.children;
+    if (listChildren) {
+      const listChildrenArray = Array.from(listChildren);
+      setList(listChildrenArray);
+    }
+  }, [listRef]);
 
   return (
     <NavbarContainer>
@@ -99,13 +104,15 @@ const Navbar = () => {
                 <div className='supporters'>
                   <div className='text'>Individual Supporters</div>
                   <div className='text'>Business Supporters</div>
-                  <div className='text'>Churches Supporters</div>
+                  <div className='text'>Church Supporters</div>
                 </div>
               </div>
             </li>
 
-            <li id='donate'>DONATE</li>
-            <li>Contact Us</li>
+            <li id='donate' className='not'>
+              DONATE
+            </li>
+            <li className='not'>Contact Us</li>
           </ul>
         </div>
       </nav>
