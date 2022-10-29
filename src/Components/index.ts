9681ef6fb6df8c82b5f64968cd02e45efdd98a91
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-29 23:19:57 +0300

    Sato

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,4 @@
 export { default as Map } from './Map/Map';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Main } from './Main/Main';
+export { default as About } from './About/About';
