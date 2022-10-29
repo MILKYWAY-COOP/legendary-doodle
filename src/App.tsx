bc925a63787f971df1211b9e86402bdf3ad6be73
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-29 23:19:44 +0300

    Sato

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { Navbar, Map, Main } from './Components';
+import { Route, Routes } from 'react-router-dom';
+import {
+  Wish,
+  Team,
+  Home,
+  Individual,
+  Church,
+  Business,
+  WDonate,
+  Directors
+} from './Pages';
 import './index.scss';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Main />
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/our-team' element={<Team />} />
+        <Route path='/directors' element={<Directors />} />
+        <Route path='/wish-list' element={<Wish />} />
+        <Route path='/why-donate' element={<WDonate />} />
+        <Route path='/individual-supporters' element={<Individual />} />
+        <Route path='/church-supporters' element={<Church />} />
+        <Route path='/business-supporters' element={<Business />} />
+      </Routes>
     </div>
   );
 }
