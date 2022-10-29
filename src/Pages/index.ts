15e824da3affa0d0529365ac206c3c559ce6f80c
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-29 23:20:17 +0300

    Sato

---
+++ src/Pages/index.ts
@@ -0,0 +1,8 @@
+export { default as Wish } from './Wish/Wish';
+export { default as Team } from './Team/Team';
+export { default as Home } from './Home/Home';
+export { default as Individual } from './Individual/Individual';
+export { default as Church } from './Church/Church';
+export { default as Business } from './Business/Business';
+export { default as WDonate } from './WDonate/WDonate';
+export { default as Directors } from './Directors/Directors';
