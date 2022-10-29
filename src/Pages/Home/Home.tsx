798dc0cd4e78326ca4f4e2be660f744f2397b166
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-29 23:20:34 +0300

    Sato

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import { Navbar, Map, Main, About } from '../../Components';
+
+export const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      <About />
+    </div>
+  );
+};
+
+export default Home;
