15bf463f3995461fb46f4cb30fc06ea9ea2297f3
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-31 19:32:25 +0300

    Progress

--- src/Components/About/About.styled.ts
+++ src/Components/About/About.styled.ts
@@ -2,29 +2,24 @@ import styled from 'styled-components';
 
 export const AboutContainer = styled.div`
   width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  h1 {
-    font-family: var(--mainFont);
-    font-size: 3rem;
-  }
+  height: 100%;
+  z-index: 9999;
 
   .bottom {
-    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
     .accordion {
+      align-items: center;
       display: flex;
-      margin: 60px auto;
-      width: 400px;
+      flex-direction: column;
+      width: 100%;
+      gap: 1em;
 
       li {
         list-style: none;
         width: 100%;
-        height: 100%;
-        margin: 20px;
         padding: 10px;
         border-radius: 8px;
         background-color: var(--lightBlue);
@@ -35,10 +30,16 @@ export const AboutContainer = styled.div`
         label {
           display: flex;
           align-items: center;
+          justify-content: center;
           padding: 10px;
           font-size: 18px;
           font-weight: 900;
           cursor: pointer;
+
+          @media (max-width: 500px) {
+            font-size: 16px;
+            padding: 5px;
+          }
 
           &::before {
             content: '+';
@@ -58,18 +59,31 @@ export const AboutContainer = styled.div`
         }
 
         .content {
-          color: var(--purePurple);
+          height: 170px;
           padding: 0 10px;
           line-height: 20px;
           max-height: 0;
           overflow: hidden;
-          transition: max-height 0.5s, padding 0.5s;
+          transition: padding 0.5s, max-height 0.5s;
+
+          @media (max-width: 500px) {
+            height: 250px;
+          }
+
+          p {
+            text-align: left;
+          }
         }
       }
     }
   }
+
   .accordion input[type='radio']:checked + label + .content {
     max-height: 400px;
     padding: 10px 10px 20px;
+  }
+
+  .accordion input[type='radio']:checked + label::before {
+    content: '-';
   }
 `;
