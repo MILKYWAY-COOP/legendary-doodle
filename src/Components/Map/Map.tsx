81f38d4f39fde5e1103c1fda98d23cb192cca73a
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-31 19:32:54 +0300

    Progress

--- src/Components/Map/Map.tsx
+++ src/Components/Map/Map.tsx
@@ -1,7 +1,38 @@
-import { useState } from 'react';
+import { MapDiv } from './Map.styled';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { googleAPIKey } from '../../auth';
+import { useMemo } from 'react';
 
-export const Map = () => {
-  const [coords, setCoords] = useState({ lat: -0.7112125, lng: 36.9930926 });
-  return <div>Map</div>;
-};
-export default Map;
+interface MapProps {
+  coordinates: {
+    lat: number;
+    lng: number;
+  };
+}
+
+function MapContainer({ coordinates }: MapProps) {
+  const { lat, lng } = coordinates;
+  const center = useMemo(() => ({ lng: lng, lat: lat }), [lat, lng]);
+  return (
+    <MapDiv>
+      <GoogleMap
+        zoom={15}
+        center={center}
+        mapContainerClassName='map-container'
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    </MapDiv>
+  );
+}
+
+export default function Map() {
+  const coords = { lat: -0.7112125, lng: 36.9930926 };
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: googleAPIKey
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <MapContainer coordinates={coords} />;
+}
