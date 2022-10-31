c2b0311d0d211251c81a1e57920c479713f979e8
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-31 19:33:06 +0300

    Progress

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,21 @@
-import { Navbar, Map, Main, About } from '../../Components';
+import { HomeContainer } from './Home.styled';
+import { Navbar, Map, Main, About, Footer } from '../../Components';
 
 export const Home = () => {
   return (
-    <div>
+    <HomeContainer>
       <Navbar />
       <Main />
-      <About />
-    </div>
+      <div className='bottom'>
+        <h1>About Koimbi Children's Home</h1>
+        <div className='MapAbout'>
+          <About />
+          <Map />
+        </div>
+        <div className='curve'></div>
+      </div>
+      <Footer />
+    </HomeContainer>
   );
 };
 
