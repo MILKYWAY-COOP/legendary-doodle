353022e28aea7591b44ad8a5792beedaf351f81d
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-31 23:47:52 +0300

    Late night commit

--- src/Pages/Church/Church.tsx
+++ src/Pages/Church/Church.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import { ChurchContainer } from './Church.styled';
 
 const Church = () => {
-  return <div>Church</div>;
+  return (
+    <ChurchContainer>
+      <h1>Church Supporters</h1>
+      <p>
+        We are so grateful to be able to collaborate with churches in our
+        community! These churches provide us with invaluable assistance,
+        enabling us to shine a beacon of hope into the lives of the young
+        children in our care. If your church would like to work with us to make
+        a difference in the lives of these children, please contact us at
+        12345678.
+      </p>
+      <ul>
+        <li>Church 1</li>
+        <li>Church 2</li>
+        <li>Church 3</li>
+        <li>Church 4</li>
+        <li>Church 5</li>
+        <li>Church 6</li>
+      </ul>
+    </ChurchContainer>
+  );
 };
 
 export default Church;
