9613b2cf75681a760844904ff6992e2cc6dcf80e
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-31 19:32:21 +0300

    Progress

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -2,3 +2,4 @@ export { default as Map } from './Map/Map';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Main } from './Main/Main';
 export { default as About } from './About/About';
+export { default as Footer } from './Footer/Footer';
