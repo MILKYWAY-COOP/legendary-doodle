3152caeef6b1996fd2369ca34d50e6e93592f0f3
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-01 22:04:56 +0300

    Final

--- src/Pages/Business/Business.tsx
+++ src/Pages/Business/Business.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { BusinessContainer } from './Business.styled';
 
 const Business = () => {
-  return <div>Business</div>;
+  return (
+    <BusinessContainer>
+      <h1>Business Supporters</h1>
+      <ul>
+        <li>Business 1</li>
+        <li>Business 2</li>
+        <li>Business 3</li>
+        <li>Business 4</li>
+        <li>Business 5</li>
+        <li>Business 6</li>
+      </ul>
+    </BusinessContainer>
+  );
 };
 
 export default Business;
