4e0bef7d80b493bbab9f0988c44d758dd8e7b344
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-01 22:03:49 +0300

    Final

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import {
   Church,
   Business,
   WDonate,
-  Directors
+  Contact,
+  Donate
 } from './Pages';
 import { Navbar, Footer } from './Components';
 import './index.scss';
@@ -19,14 +20,28 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/our-team' element={<Team />} />
-        <Route path='/directors' element={<Directors />} />
         <Route path='/wish-list' element={<Wish />} />
         <Route path='/why-donate' element={<WDonate />} />
         <Route path='/individual-supporters' element={<Individual />} />
         <Route path='/church-supporters' element={<Church />} />
         <Route path='/business-supporters' element={<Business />} />
+        <Route path='/contact-us' element={<Contact />} />
+        <Route path='/donate' element={<Donate />} />
       </Routes>
       <Footer />
+      <div className='milkyway'>
+        <p>
+          &copy;2022. A{' '}
+          <a
+            href='https://milkyway-coop.github.io/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            MILKYWAY
+          </a>{' '}
+          PRODUCTION. All rights reserved
+        </p>
+      </div>
     </div>
   );
 }
