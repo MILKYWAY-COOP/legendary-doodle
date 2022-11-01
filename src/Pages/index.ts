9808ae7d1331a25f15382bb7b096e668d93e1e23
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-01 22:04:36 +0300

    Final

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -5,4 +5,5 @@ export { default as Individual } from './Individual/Individual';
 export { default as Church } from './Church/Church';
 export { default as Business } from './Business/Business';
 export { default as WDonate } from './WDonate/WDonate';
-export { default as Directors } from './Directors/Directors';
+export { default as Contact } from './Contact/Contact';
+export { default as Donate } from './Donate/Donate';
