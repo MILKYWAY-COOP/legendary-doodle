3da54291b7cc5bea1cd247923dfc3a468cfa69cb
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-01 22:05:38 +0300

    Final

--- src/Pages/Team/Team.tsx
+++ src/Pages/Team/Team.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { TeamContainer } from './Team.styled';
 
 export const Team = () => {
   return (
-    <div>Team</div>
-  )
-}
+    <TeamContainer>
+      <h1>Our Team</h1>
+      <p>
+        A team of well-wishers provides 24-hour care for the kids at Koimbi
+        Children's Home. This team is responsible for many different things.
+        They include, but are not limited to, picking up and dropping off kids
+        at school, assisting with schoolwork, preparing meals, and creating an
+        atmosphere that values respect, accountability, and empowerment.
+      </p>
+      <p>
+        It takes a lot of discipline, love, patience, and compassion to be a
+        team member. Although it may seem easy, providing love, care, and
+        encouragement for these young ones is one of the most difficult aspect
+        of our work. There are various challenges involved in taking in and
+        caring for abused and neglected children. Some of them have been the
+        “mom” or “dad” to younger siblings. Many kids have never experienced
+        love or even understood what love truly is, while others have have never
+        never had life-rules or structure. It takes time, effort, and patience
+        to win their trust; it is merely the beginning of a long process with
+        these kids. It is beyond rewarding to be able to participate in their
+        lives. Being a Team Member is rewarding since they get to contribute in
+        some tangible way to their success. We can say with confidence that our
+        team is made up of a remarkable group of committed, strong, and
+        compassionate individuals who give their all to enhancing the lives of
+        the kids in our care. We are incredibly grateful for everything they do.
+      </p>
+      <div className='curve'></div>
+    </TeamContainer>
+  );
+};
 
-export default Team
+export default Team;
