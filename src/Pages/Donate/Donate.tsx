31282a38900364579e687d308246a91973624da0
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-01 22:05:19 +0300

    Final

---
+++ src/Pages/Donate/Donate.tsx
@@ -0,0 +1,17 @@
+import { DonateContainer } from './Donate.styled';
+
+const Donate = () => {
+  return (
+    <DonateContainer>
+      <h1>Donate</h1>
+      <div className='img'></div>
+      <h4>Mpesa Till Number:</h4>
+      <h4>#######</h4>
+      <h4>KOIMBI CHILDRENS HOME</h4>
+      <p>Asanti</p>
+      <div className='curve'></div>
+    </DonateContainer>
+  );
+};
+
+export default Donate;
