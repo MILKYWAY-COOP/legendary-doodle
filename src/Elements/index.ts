392dfb2f7ea062ddbaf358659c0c6b0775a1fead
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-01 22:04:20 +0300

    Final

--- src/Elements/index.ts
+++ src/Elements/index.ts
@@ -1,9 +1,15 @@
 import img4 from './imgs/img4.jpg';
 import hold from './imgs/hold.svg';
 import board from './imgs/Board.jpg';
+import list from './imgs/list.svg';
+import close from './imgs/x-lg.svg';
+import mpesa from './imgs/mpesa.png'
 
 export const Files = {
   img4,
   hold,
-  board
+  board,
+  list,
+  close,
+  mpesa
 };
