4a346c2a7e7b67015fa01c29002e383aa2ba059e
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-01 22:04:12 +0300

    Final

--- src/Components/Navbar/Navbar.styled.ts
+++ src/Components/Navbar/Navbar.styled.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
+import { Files } from '../../Elements';
+
+const { list, close } = Files;
 
 export const NavbarContainer = styled.div`
+  position: relative;
   nav {
     display: flex;
     flex-direction: row;
@@ -38,6 +42,14 @@ export const NavbarContainer = styled.div`
       }
     }
 
+    .mobile-nav-toggle {
+      display: none;
+      outline: none;
+      border: none;
+      cursor: pointer;
+      transition: all 0.3s ease-in-out;
+    }
+
     .navLinks {
       margin-right: 2rem;
       position: relative;
@@ -47,7 +59,7 @@ export const NavbarContainer = styled.div`
         width: 100px;
         height: 100px;
         position: absolute;
-        background: #fff;
+        background: var(--brightRed);
         border-radius: 4px;
         box-shadow: 0 50px 100px rgba(50, 50, 93, 0.1),
           0 15px 35px rgba(50, 50, 93, 0.15), 0 5px 15px rgba(0, 0, 0, 0.1);
@@ -65,7 +77,7 @@ export const NavbarContainer = styled.div`
           width: 20px;
           height: 20px;
           display: block;
-          background: white;
+          background: var(--brightRed);
           transform: translateY(-50%) rotate(45deg);
           left: 20%;
         }
@@ -111,7 +123,6 @@ export const NavbarContainer = styled.div`
           .dropdown {
             opacity: 0;
             position: absolute;
-            overflow: hidden;
             padding: 20px;
             top: -70px;
             border-radius: 2px;
@@ -139,6 +150,7 @@ export const NavbarContainer = styled.div`
                 text-overflow: clip;
                 padding: 10px 0;
                 transition: all 0.2s ease;
+                color: var(--cloud);
 
                 &:hover {
                   text-decoration: underline;
@@ -174,15 +186,141 @@ export const NavbarContainer = styled.div`
         }
 
         #donate {
-          background-color: var(--brightRed);
-          padding: 0.5rem 1rem;
+          background-color: var(--purePurple);
+          padding: 0.8rem;
           border-radius: 0.2rem;
           color: var(--cloud);
+          cursor: pointer;
+        }
+      }
+    }
+
+    @media (max-width: 900px) {
+      .mobile-nav-toggle {
+        position: absolute;
+        display: block;
+        z-index: 9999;
+        background-color: var(--purePurple);
+        background-image: url(${list});
+        background-repeat: no-repeat;
+        background-position: center;
+        width: 2.5em;
+        height: 2.5em;
+        border-radius: 35%;
+        padding: 10px;
+        right: 20px;
+      }
+
+      .mobile-nav-toggle[aria-expanded='true'] {
+        background-image: url(${close});
+      }
+
+      .navLinks {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        height: fit-content;
+        position: fixed;
+        text-align: center;
+        z-index: 9000;
+        right: 0;
+        top: 0;
+        list-style: none;
+        padding: 2rem;
+        flex-direction: column;
+        transform: translateX(115%);
+        transition: transform 0.7s ease-in-out;
+        width: fit-content;
+        background-color: var(--cloud);
+        backdrop-filter: blur(5px);
+        border-radius: 10px;
+        margin: 0;
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
+        .dropdownBackground {
+          display: none;
+        }
+
+        ul {
+          display: flex;
+          flex-direction: column;
+          z-index: 9999;
+          width: 200px;
+          position: static;
+
+          li {
+            width: 100%;
+            padding: 0.5rem 0;
+            display: flex;
+            flex-direction: column;
+
+            span {
+              position: static;
+              width: 100%;
+              height: 70px;
+              text-align: left;
+              display: inline;
+              font-size: 2rem;
+              color: var(--brightRed);
+              text-decoration: underline;
+
+              .dropper {
+                display: none;
+              }
+            }
+
+            .dropdown {
+              display: flex;
+              opacity: 1;
+              padding: 10px;
+
+              .supporters,
+              .involved,
+              .about {
+                position: static;
+                width: 100%;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                /* padding: 10px; */
+
+                .text {
+                  width: 100%;
+                  height: 25px;
+                  font-size: 1rem;
+                  text-align: left;
+                  padding-left: 10px;
+                  color: black;
+                }
+              }
+            }
+          }
+
+          #donate {
+            position: static;
+            width: 100%;
+            padding: 0;
+            height: 2rem;
+            margin-top: 3rem;
+            font-size: 2rem;
+            text-align: left;
+            background: none;
+            text-decoration: underline;
+            color: var(--brightRed);
+          }
+
+          #contact {
+            color: var(--brightRed);
+            text-align: left;
+            font-size: 2rem;
+            text-decoration: underline;
+          }
         }
       }
 
-      @media (max-width: 900px) {
-        display: none;
+      .navLinks[data-visible='true'] {
+        transform: translateX(0%);
       }
     }
   }
