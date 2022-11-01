7b7cbce8917775b48f0098952d2a84d94f1764e3
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-01 22:08:41 +0300

    devops

--- src/auth.ts
+++ src/auth.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 
 //export const googleAPIKey = 'AIzaSyDbuVBC5I-97jCizmKpK2MIwQmiD5sUh2E';
 //export const googleAPIKey = 'AIzaSyDmTkkVZ-ELq-_zksOUdZNBc-BxTo19rsI';
