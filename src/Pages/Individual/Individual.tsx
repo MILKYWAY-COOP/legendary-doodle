c1c913427baa9323d843fa1655076d884f843048
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-01 22:05:30 +0300

    Final

--- src/Pages/Individual/Individual.tsx
+++ src/Pages/Individual/Individual.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import { IndividualContainer } from './Individual.styled';
 
 const Individual = () => {
-  return <div>Individual</div>;
+  return (
+    <IndividualContainer>
+      <h1>Individual Supporters</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>Individual 1</td>
+            <td>Individual 2</td>
+            <td>Individual 3</td>
+          </tr>
+          <tr>
+            <td>Individual 1</td>
+            <td>Individual 2</td>
+            <td>Individual 3</td>
+          </tr>
+          <tr>
+            <td>Individual 1</td>
+            <td>Individual 2</td>
+            <td>Individual 3</td>
+          </tr>
+          <tr>
+            <td>Individual 1</td>
+            <td>Individual 2</td>
+            <td>Individual 3</td>
+          </tr>
+          <tr>
+            <td>Individual 1</td>
+            <td>Individual 2</td>
+            <td>Individual 3</td>
+          </tr>
+          <tr>
+            <td>Individual 1</td>
+            <td>Individual 2</td>
+            <td>Individual 3</td>
+          </tr>
+          <tr>
+            <td>Individual 1</td>
+            <td>Individual 2</td>
+            <td>Individual 3</td>
+          </tr>
+          <tr>
+            <td>Individual 1</td>
+            <td>Individual 2</td>
+            <td>Individual 3</td>
+          </tr>
+          <tr>
+            <td>Individual 1</td>
+            <td>Individual 2</td>
+            <td>Individual 3</td>
+          </tr>
+        </tbody>
+      </table>
+    </IndividualContainer>
+  );
 };
 
 export default Individual;
