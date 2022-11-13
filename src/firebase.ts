74f48155bbad20fd1d883e2fd0b780829ad2d2b6
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-13 18:20:05 +0300

    Ok

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyBgV6k9SicGfL2nH9VI78cE4NAYxX0FVkM',
-  authDomain: 'koimbi-f5e3a.firebaseapp.com',
-  projectId: 'koimbi-f5e3a',
-  storageBucket: 'koimbi-f5e3a.appspot.com',
-  messagingSenderId: '163854662774',
-  appId: '1:163854662774:web:554a7bbee8695892753357',
-  measurementId: 'G-ZXYGPZE31H'
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyBgV6k9SicGfL2nH9VI78cE4NAYxX0FVkM',
+//   authDomain: 'koimbi-f5e3a.firebaseapp.com',
+//   projectId: 'koimbi-f5e3a',
+//   storageBucket: 'koimbi-f5e3a.appspot.com',
+//   messagingSenderId: '163854662774',
+//   appId: '1:163854662774:web:554a7bbee8695892753357',
+//   measurementId: 'G-ZXYGPZE31H'
+// };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 //export const googleAPIKey = 'AIzaSyDbuVBC5I-97jCizmKpK2MIwQmiD5sUh2E';
 //export const googleAPIKey = 'AIzaSyDmTkkVZ-ELq-_zksOUdZNBc-BxTo19rsI';
